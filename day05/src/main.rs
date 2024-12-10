51bfc77cb66e39cc6958ca34ac550927ec2b3fa6
Anton Amirian <extuessl@gmail.com>
2024-12-10 13:33:02 +0200

    day 5 part 2 but one test fails

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -4,12 +4,16 @@ fn main() {
     let file_contents = std::fs::read_to_string("input.txt")
         .expect("err");
 
-    let res = check_rules(&file_contents);
+    let res = check_rules(&file_contents, false);
 
     println!("part 1 result: {}", res);
+
+    let res = check_rules(&file_contents, true);
+
+    println!("part 2 result: {}", res);
 }
 
-#[derive(Clone,Copy)]
+#[derive(Clone,Copy,Debug)]
 struct Rule {
     x: Page,
     y: Page,
@@ -22,6 +26,7 @@ struct SafetyManual(Vec<Rule>, Vec<Book>);
 
 trait CheckValidity {
     fn check_validity(self, book: Book) -> bool;
+    fn correct(self, book: Book) -> Book;
 }
 
 impl CheckValidity for Rule {
@@ -33,9 +38,28 @@ impl CheckValidity for Rule {
             _ => true
         }
     }
+
+    fn correct(self, book: Book) -> Book {
+        let page_x = book.iter().position(|p| *p == self.x);
+        let page_y = book.iter().position(|p| *p == self.y);
+        let mut book = book.clone();
+
+        match (page_x, page_y) {
+            (Some(x), Some(y)) => {
+                if x > y {
+                    book.swap(x,y);
+                }
+                book
+            },
+            _ => {
+                book
+            }
+        }
+
+    }
 }
 
-fn check_rules(input: &str) -> u32 {
+fn check_rules(input: &str, correct: bool) -> u32 {
     let prog = get_update_record(input);
     let rules = prog.0;
     let mut total: Page = 0;
@@ -44,9 +68,18 @@ fn check_rules(input: &str) -> u32 {
         let valid = rules.iter()
             .all(|rule| rule.check_validity(book.to_vec()));
 
-        if valid {
+        if valid && !correct {
             let mid_index = book.len() / 2;
             let mid_value = book[mid_index];
+            total = total + mid_value;
+        }
+        else if correct {
+            let mut new_entry = book.clone();
+            for rule in rules.iter() {
+                new_entry = rule.correct(new_entry.clone());
+            }
+            let mid_index = new_entry.len() / 2;
+            let mid_value = new_entry[mid_index];
             total = total + mid_value;
         }
     }
@@ -67,7 +100,7 @@ fn get_update_record(input: &str) -> SafetyManual {
                 .map(|x| {
                     match x.trim().parse::<Page>() {
                         Ok(value) => value,
-                        Err(err) => 0
+                        Err(_) => 0
                     }
                 })
                 .collect();
@@ -80,11 +113,11 @@ fn get_update_record(input: &str) -> SafetyManual {
             }
         }
         else {
-            let nums: Vec<u32> = line.split(",")
+            let nums: Vec<Page> = line.split(",")
                     .map(|x| {
                         match x.trim().parse::<Page>() {
                             Ok(value) => value,
-                            Err(err) => 0
+                            Err(_) => 0
                         }
                     })
                     .collect();
@@ -135,7 +168,7 @@ mod tests {
             97,13,75,29,47
         "#;
 
-        assert_eq!(check_rules(input), 143);
+        assert_eq!(check_rules(input, false), 143);
     }
 
     #[test]
@@ -166,7 +199,7 @@ mod tests {
             75,47,61,53,29
         "#;
 
-        assert_eq!(check_rules(input), 61);
+        assert_eq!(check_rules(input, false), 61);
     }
 
     #[test]
@@ -199,6 +232,93 @@ mod tests {
             75,29,13
         "#;
 
-        assert_eq!(check_rules(input), 143);
+        assert_eq!(check_rules(input, false), 143);
+    }
+
+    #[test]
+    fn test_p2() {
+        let input = r#"
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+        "#;
+
+        assert_eq!(check_rules(input, true), 123);
+    }
+
+    #[test]
+    fn test_p2_single() {
+        let input = r#"
+            29|13
+
+            61,13,29
+        "#;
+
+        assert_eq!(check_rules(input, true), 29);
+    }
+
+    #[test]
+    fn test_p2_single2() {
+        let input = r#"
+            97|75
+            47|53
+
+            75,97,47,61,53
+        "#;
+
+        assert_eq!(check_rules(input, true), 47);
+    }
+
+    #[test]
+    fn test_p2_single3() {
+        let input = r#"
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            97,13,75,29,47
+        "#;
+
+        assert_eq!(check_rules(input, true), 47);
     }
 }
