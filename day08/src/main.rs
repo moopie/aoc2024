b60a5bc694ff2561437f3243e9a9fc5a36c3e4b7
Anton Amirian <extuessl@gmail.com>
2024-12-13 04:19:04 +0200

    some more code for day 8

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,11 +1,17 @@
-use std::{borrow::Borrow, collections::HashSet};
+use std::collections::HashSet;
+
+#[derive(Eq, PartialEq, Hash, Debug)]
+struct Distance {
+    distance: u64,
+    direction: u64
+}
 
 fn get_node_count(str_map: String) -> u64 {
     let lines = str_map.lines();
 
     let freqs: HashSet<char> = str_map.chars()
         // remove empty space
-        .filter(|x| *x != ',')
+        .filter(|x| *x != '.')
         .collect();
 
     let map: Vec<Vec<char>> = lines
@@ -14,13 +20,32 @@ fn get_node_count(str_map: String) -> u64 {
         .collect();
 
     for freq in freqs {
-        let mut freq_locs: Vec<(u32, u32)> = vec![];
+        let mut freq_locs: Vec<(f64, f64, char)> = vec![];
 
         for (y, line) in map.iter().enumerate() {
             for (x, ch) in line.iter().enumerate() {
                 if freq == *ch {
-                    freq_locs.push((x as u32, y as u32));
+                    freq_locs.push((x as f64, y as f64, ch.clone()));
                 }
+            }
+        }
+
+        let mut set: HashSet<Distance> = HashSet::new();
+        for (x1, y1, _) in freq_locs.iter() {
+            for (x2, y2, ch) in freq_locs.iter() {
+                if x1 == x2 && y1 == y2 {
+                    continue;
+                }
+
+                let dist = ((x1 - y1).abs() + (y1 - y2).abs()) as f64;
+                let dir = (y2 - y1).atan2(x2 - x1).to_degrees();
+
+                set.insert(Distance {
+                    distance: dist.abs() as u64,
+                    direction: dir.abs() as u64
+                });
+
+                println!("'{}'\tdist: {}\t dir {}", ch, dist.abs() as u64, dir.abs() as u64);
             }
         }
     }
