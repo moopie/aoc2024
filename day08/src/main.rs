703e25b6adbf45b93ad814c486605d0e1f1d302e
Anton Amirian <extuessl@gmail.com>
2024-12-16 01:29:39 +0200

    something is still wrong here, i need to check the math

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,17 +1,15 @@
 use std::collections::HashSet;
+use std::fs::read_to_string;
 
-#[derive(Eq, PartialEq, Hash, Debug)]
-struct Distance {
-    distance: usize,
-    direction: usize
-}
-
-fn get_node_count(str_map: String) -> u64 {
+fn get_node_count(str_map: String) -> i64 {
     let lines = str_map.lines();
 
     let freqs: HashSet<char> = str_map.chars()
         // remove empty space
-        .filter(|x| *x != '.')
+        .filter(|x|
+            *x != '.' &&
+            *x != '\n' &&
+            *x != ' ')
         .collect();
 
     let map: Vec<Vec<char>> = lines
@@ -19,41 +17,60 @@ fn get_node_count(str_map: String) -> u64 {
         .filter(|x: &Vec<char>| !x.is_empty())
         .collect();
 
+    let lenx = map[0].len() as i64;
+    let leny = map.len() as i64;
+    let mut set: HashSet<(i64, i64)> = HashSet::new();
+    let mut freq_locs: Vec<(i64, i64, char)> = vec![];
+
     for freq in freqs {
-        let mut freq_locs: Vec<(usize, usize, char)> = vec![];
-
-        for (y, line) in map.iter().enumerate() {
-            for (x, ch) in line.iter().enumerate() {
-                if freq == *ch {
-                    freq_locs.push((x, y, ch.clone()));
+        for x  in 0..map.len() {
+            for y in 0..map[x].len() {
+                if freq == map[y][x] {
+                    freq_locs.push((x as i64, y as i64, freq.clone()));
                 }
-            }
-        }
-
-        let mut set: HashSet<Distance> = HashSet::new();
-        for (x1, y1, ch1) in freq_locs.iter() {
-            println!("for '{}' ({},{}) distances are:", ch1, x1, y1);
-            for (x2, y2, ch2) in freq_locs.iter() {
-                if x1 == x2 && y1 == y2 {
-                    continue;
-                }
-
-                let dist = x1.abs_diff(*x2) + y1.abs_diff(*y2);
-
-                set.insert(Distance {
-                    distance: dist,
-                    direction: 0
-                });
-
-                println!("\t'{}' ({},{})\tdist: {}", ch2, x2, y2, dist);
             }
         }
     }
-    0
+
+    for (x, (x1, y1, _)) in freq_locs.iter().enumerate() {
+        // the set of other frequency towers
+
+        for y in x..freq_locs.len() {
+            let (x2, y2, _) = freq_locs[y];
+
+            if *x1 == x2 && *y1 == y2 {
+                continue;
+            }
+
+            let dx = (x2 - x1).abs();
+            let dy = (y2 - y1).abs();
+
+            let ax = x1 - dx;
+            let ay = y1 - dy;
+
+            let bx = x2 + dx;
+            let by = y2 + dy;
+
+            if ax >= 0 && ax < lenx && ay >= 0 && ay < leny {
+                set.insert((ax, ay));
+            }
+            if bx >= 0 && bx < lenx && by >= 0 && by < leny {
+                set.insert((bx, by));
+            }
+        }
+    }
+    println!("\nset: {:?}", set);
+    set.len() as i64
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("AOC 2024 day 8!");
+
+    let input = read_to_string("input.txt").expect("err");
+
+    let node_count = get_node_count(input);
+
+    println!("Part 1 solution: {}", node_count);
 }
 
 #[cfg(test)]
